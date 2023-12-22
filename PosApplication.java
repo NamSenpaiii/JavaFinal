dcdbcffc823a2d13dedae18b20f6b1988d055e8c
Nhat Nam <69340036+NamSenpaiii@users.noreply.github.com>
2023-12-22 22:47:55 +0700

    Add files via upload
    Templates + Models,Controllers,Services

---
+++ PosApplication.java
@@ -0,0 +1,14 @@
+package finalproject.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosApplication.class, args);
+    }
+
+}
