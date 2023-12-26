8c88d7f19e985cc53510294aa459cfd404fc36c0
Nhat Nam <69340036+NamSenpaiii@users.noreply.github.com>
2023-12-26 23:38:27 +0700

    Delete pos directory

--- pos/Models/User/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.javafinal.Model;
-
-public class CustomerNotFoundException extends Throwable{
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
--- pos/Models/User/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.javafinal.Model;
-
-public class ProductNotFoundException extends Throwable {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
-
